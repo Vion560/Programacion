4e50a1bccd593bcae79aa5b1b647f3389927e722
Vion560 <113586244+Vion560@users.noreply.github.com>
2022-10-10 10:28:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace bucleejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter;
+            int number;
+            counter = 1;
+            Console.WriteLine("Escriba un número");
+            number = int.Parse(Console.ReadLine());
+            while (counter < number)
+            {
+                Console.WriteLine(counter);
+                counter = counter + 1;
+            }
+        }
+    }
+}
