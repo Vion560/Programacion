bf3ddda61d93f8a384b06607bc980c71ccca13ef
Vion560 <113586244+Vion560@users.noreply.github.com>
2022-10-10 11:08:50 +0200

    Add files via upload

---
+++ ejbucle4.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace ejbucle4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int number2;
+            Console.WriteLine("Write number");
+            number = int.Parse(Console.ReadLine());
+            while (number >= 0)
+            {
+                number2 = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("el numero es negativo")
+        }   
+    }
