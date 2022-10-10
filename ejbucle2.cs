f00f14d81f42c7da3a6eba53cb987cb00848e708
Vion560 <113586244+Vion560@users.noreply.github.com>
2022-10-10 11:08:17 +0200

    Add files via upload

---
+++ ejbucle2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ejbucle2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter;
+            counter = 1;
+            while (counter < 100)
+            {
+                Console.WriteLine(counter);
+                counter = counter + 5;
+            }
+            Console.WriteLine("FIN");
+        }
+    }
+}
