fc78a9c4dcea9d07a0eb08881f2e35c44abe5327
Vion560 <113586244+Vion560@users.noreply.github.com>
2022-10-17 11:04:40 +0200

    Add files via upload

---
+++ ejercicio1funciones.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            b = 5;
+
+            Multiplos(b);
+        }
+        static int Multiplos(int num)
+        {
+            //num es el numero que se queda guardado de b(5)
+            int a;
+            int multiplicador;
+            multiplicador = 1;
+            do
+            {
+                a = int.Parse(Console.ReadLine());
+                if ((a % num) == 0)
+                {
+                    multiplicador = multiplicador * a;
+                }               
+            }
+            while (a != 0);
+            { return a; }
+        }                  
+    }
+}
