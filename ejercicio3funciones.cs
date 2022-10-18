b04325719b46746ffe7c267f6ac1bf01a358119c
Vion560 <113586244+Vion560@users.noreply.github.com>
2022-10-18 09:49:00 +0200

    Add files via upload

---
+++ ejercicio3funciones.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int base = ConvertToInt32(Console.ReadLine());
+            int exp = ConvertToInt32(Console.ReadLine());
+            Console.WriteLine("El resultado es"+ Power2(base,exp))
+        }
+        static float Power2(int _base, int exp)
+        {
+            if (exp == 0)
+            {
+                return 1;
+            }
+
+            if(exp > 0)
+            {
+                //potencia
+                int total = _base;
+                int counter = 1;
+                while( counter < exp)
+                {
+                    total = total * _base;
+                    counter = counter + 1;
+                }
+                return total;
+            }
+        }
+    }
+}
