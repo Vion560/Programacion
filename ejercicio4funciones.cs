267adfacb8e4320c2855bbdae805453013c6a95b
Vion560 <113586244+Vion560@users.noreply.github.com>
2022-10-18 10:19:38 +0200

    Add files via upload

---
+++ ejercicio4funciones.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ejercicio5funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Esprimo();
+        }
+        static void Esprimo()
+        {
+            int num;
+            num = 7;
+            int counter;
+            counter = 2;
+            bool prime;
+            prime = true;
+            do
+            {
+                if(num % counter == 0)
+                {
+                    prime = false;
+                }
+                else
+                {
+                    counter = counter + 1;
+                };
+            }
+            while (counter <= num);
+        }
+    }
+}
