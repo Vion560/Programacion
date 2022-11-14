a94a5f6c8c730017d28281795b0a6c453fa2d8d3
Vion560 <113586244+Vion560@users.noreply.github.com>
2022-11-14 09:20:31 +0100

    Add files via upload

---
+++ Programej2array.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ej2();
+        }
+        static void ej2()
+        {
+            Random r = new Random();
+                int[] array = new int[15];
+            for(int i=0; i < array.Length; ++i)
+            {
+                array[i] = r.Next(0, 50);
+                Console.WriteLine(array[i]);
+            }
+            //sacar el valor mas alto
+            int higher = array[0];
+            int lower = array[1];
+            for (int i = 0; i< array.Length;++i)
+            {
+                if(array[i] > higher)
+                {
+                    higher = array[i];
+                }
+                if (array[i] < lower)
+                {
+                    lower = array[i];
+                }
+            }
+            Console.WriteLine("el mayor es: " + higher + "el menor es: " + lower);
+        }
+    }
+}
